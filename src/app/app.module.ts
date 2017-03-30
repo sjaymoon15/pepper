ca79fadec27f22c762bc9164cb4144b4f57e557c
Seungjae Moon <sjaymoon15@gmail.com>
2017-03-30 11:42:34 -0700

    integrate angularFire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDYi882vuOnrUBpMUlxc9-QEKr8rPWCCWQ",
+    authDomain: "pepper-8b98c.firebaseapp.com",
+    databaseURL: "https://pepper-8b98c.firebaseio.com",
+    storageBucket: "pepper-8b98c.appspot.com",
+    messagingSenderId: "491345027329"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +21,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
