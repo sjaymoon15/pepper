e239169a906750afc7b3964a2a27824904d73656
Seungjae Moon <sjaymoon15@gmail.com>
2017-04-03 12:14:06 -0700

    learn simple firebase join

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +11,23 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
 export class AppComponent {
   title = 'app works!';
   cuisines: FirebaseListObservable<any[]>;
-  restaurants: FirebaseListObservable<any[]>;
+  restaurants: Observable<any[]>;
 
   constructor(private af: AngularFire){
   }
 
   ngOnInit() {
     this.cuisines = this.af.database.list('/cuisines'); 
-    this.restaurants = this.af.database.list('/restaurants');
+    this.restaurants = this.af.database.list('/restaurants')
+      .map(restaurants => {
+        console.log('before map', restaurants);
+        restaurants.map(restaurant => {
+          restaurant.cuisineType = this.af.database.object('/cuisines/' + restaurant.cuisine)
+        });
+        console.log('after map', restaurants);
+
+        return restaurants;
+      });
   }
 
 }
